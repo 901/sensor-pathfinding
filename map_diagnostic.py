e7e03c350a3b2c11b59f5d35bf4ad93f894a46d1
Lawrence Maceren <lawrencemaceren96@gmail.com>
2017-04-11 15:11:54 -0400

    Filtering sorta working?

--- map_diagnostic.py
+++ map_diagnostic.py
@@ -46,7 +46,7 @@ grid = [[GridCell('N') for y in range(GridRows)] for x in range(GridCols)]
 # Make Random Grid
 def makeGrid(withBlocks):
 	heatmap = [[0 for y in range(GridRows)] for x in range(GridCols)]
-	
+
 	# Highways
 	needtoblock = GridCols*GridRows*0.2
 	blocked = 0
@@ -139,23 +139,23 @@ def drawScreen(GridSurface,HeatSurface,time,location,transitions,sensing,start_x
 	# Draw grid and cursor
 	GameScreen.blit(GridSurface,(0,0))
 	#pygame.draw.rect(GameScreen, (255,0,0), (cursor_x*blockwidth+9,cursor_y*blockwidth+9,blockwidth+2,blockwidth+2), 2)
-	
+
 	# Draw heatmap
 	HeatSurface.set_alpha(128)
 	GameScreen.blit(HeatSurface,(0,0))
-	
+
 	# Draw start and end point
 	#pygame.draw.circle(GameScreen, (255,0,0), (start_x*blockwidth+blockwidth/2+10,start_y*blockwidth+blockwidth/2+10),blockwidth/2, 0)
 	#pygame.draw.circle(GameScreen, (0,255,0), (goal_x*blockwidth+blockwidth/2+10,goal_y*blockwidth+blockwidth/2+10),blockwidth/2, 0)
-	
+
 	# Draw current agent location
 	if location != []:
 		pygame.draw.circle(GameScreen, (0,255,0), (location[time][0]*blockwidth+blockwidth/2+10,location[time][1]*blockwidth+blockwidth/2+10),blockwidth/2, 0)
-	
+
 	# Draw text
 	label = myfont.render("G = New Map, E = New Start, S = Save, L = Load", 1, (0,0,0))
 	GameScreen.blit(label, (20, blockwidth*GridRows+14))
-	
+
 	# Draw time
 	label = myfont.render("t = "+str(time), 1, (0,0,0))
 	GameScreen.blit(label, (10+blockwidth*GridCols+20, 30))
@@ -164,12 +164,12 @@ def drawScreen(GridSurface,HeatSurface,time,location,transitions,sensing,start_x
 	if transitions != []:
 		label = myfont.render("Action: "+str(transitions[time]), 1, (0,0,0))
 		GameScreen.blit(label, (10+blockwidth*GridCols+20, 50))
-	
+
 	# Draw sensing
 	if sensing != []:
 		label = myfont.render("Sense: "+str(sensing[time]), 1, (0,0,0))
 		GameScreen.blit(label, (10+blockwidth*GridCols+20, 70))
-	
+
 	# Draw screen
 	pygame.display.update()
 
@@ -195,7 +195,7 @@ def ground_truth_data1(s):
 	with open("ground_truth.txt", "w") as f:
 		# Write start
 		f.write("(" + str(agentx) + "," + str(agenty) + ")\n")
-		
+
 		# Iterate
 		for c in s:
 			print c,
@@ -356,19 +356,19 @@ def ground_truth_data1(s):
 # Forward Algorithm (Question D)
 def forwardAlgorithm(transition,sensing):
 	heatmap = initHeatMap()
-	
+
 	# t = 0 is the initial heatmap, where every cell has an equal probability
 	# heatmap[t=1] is after the first action and first sensing
-	
+
 	t = 1
-	
+
 	while t <= 100:
 		# Inherit old beliefs
 		#heatmap[t] = heatmap[t-1]
 		heatmap[t] = [[heatmap[t-1][x][y] for y in range(GridRows)] for x in range(GridCols)]
-		
+
 		#print str(t) + ": [" + str(len(heatmap[t])) + "][" + str(len(heatmap[t][0])) + "]"
-		
+
 		# Iterate grid
 		for y in range(GridRows):
 			for x in range(GridCols):
@@ -376,7 +376,7 @@ def forwardAlgorithm(transition,sensing):
 				# Get action and new state coordinates
 				new_x = x
 				new_y = y
-				
+
 				if transition[t] == 'L':
 					new_x = x-1
 				elif transition[t] == 'R':
@@ -385,83 +385,44 @@ def forwardAlgorithm(transition,sensing):
 					new_y = y-1
 				elif transition[t] == 'D':
 					new_y = y+1
-				
+
 				oldprob = heatmap[t-1][x][y]
 				oldprob_not = 1 - oldprob
-				
+
 				# Calculate observation model
 				celltype = grid[x][y].getType()
-				
+
 				if celltype == sensing[t]:
-					obs = 0.9
+					newprob = 0.9 * oldprob
+					newprob_not = 0.1 * oldprob_not
 				else:
-					obs = 0.1
-				
-				# Calculate transition model
+					newprob = 0.1 * oldprob
+					newprob_not = 0.9 * oldprob_not
+
+				# Calculate transition model and prior belief
 				# Probability of ending in state s' given action a in state s
-				if new_x < 0 or new_y < 0 or new_x >= GridCols or new_y >= GridRows:		# Out of bounds
-					newprob = 1
-				elif grid[new_x][new_y].getType() == 'B': # Hitting wall
-					newprob = 1
+				if new_x < 0 or new_y < 0 or new_x >= GridCols or new_y >= GridRows:		# Hitting wall or out of bounds
+					newprob = 1 * newprob
+				elif grid[new_x][new_y].getType() == 'B':
+					newprob = 1 * newprob
 				elif grid[x][y].getType() == 'B':		# On a wall
 					heatmap[t][x][y] = 0
 				else:	# Normal cell
+					'''if t == 2:
+						print str(heatmap[t-1])
+						print str(heatmap[t-1][new_x])
+						print str(heatmap[t-1][new_x][new_y])
+						#print "Old Belief: " + str(heatmap[t-1][new_x][new_y])'''
 					#print "Normal cell"
-					newprob = 0.9
+					newprob = 0.9 * newprob
+					newprob_not = 0.1 * newprob_not
 					#heatmap[t][x][y] = 0.1 * heatmap[t-1][x][y]
-				
-				# Apply prior belief
-				newprob_not = 1 - newprob
-				
-				newprob = newprob * oldprob + newprob_not + oldprob_not
-				newprob_not = newprob_not * oldprob + (1 - newprob_not) * oldprob_not
-				
-				# Multiply by observation model
-				newprob = obs * newprob
-				newprob_not = (1 - obs) * newprob_not
-				
+
 				#print "Old: <" + str(oldprob) + "," + str(oldprob_not) + "> ... New: <" + str(newprob/(newprob+ newprob_not)) + "," + str(newprob_not/(newprob + newprob_not)) + ">"
 				heatmap[t][x][y] = newprob/(newprob + newprob_not)
-				
+
 		t += 1
 	return heatmap
-
-# Error Calculation for Filtering (Question E)
-def error_filtering(heatmap,location):
-	print "Check error for filtering"
-	
-	with open("error_filtering.txt", "w") as f:
-		
-		# Take readings, ignore first 5
-		t = 6
-		while t <= 100:
-			# Find probability of cell where agent is actually located
-			true_x = location[t][0]
-			true_y = location[t][1]
-			
-			prob_true_location = heatmap[t][true_x][true_y]
-			
-			# Find cell with max probability
-			max_prob = 0
-			max_x = 0
-			max_y = 0
-			for y in range(GridRows):
-				for x in range(GridCols):
-					if heatmap[t][x][y] > max_prob:
-						max_x = x
-						max_y = y
-						max_prob = heatmap[t][x][y]
-				
-			# Calculate distance (euclidean)
-			distance = math.sqrt((true_x - max_x)**2+(true_y - max_y)**2)
-			
-			# Write to file (t, distance, probability) 
-			f.write(str(t) + "," + str(distance) + "," + str(prob_true_location) + "\n")
-			t += 1
-		
-		f.close()
-	
-	print "Wrote error to file"
 
 # Make and Draw Grid
 GridSurface = pygame.Surface(GameScreen.get_size())
@@ -501,13 +462,13 @@ while(running):
 				makeGrid(True)
 				GridSurface = drawGrid(GridSurface)
 				agentx,agenty = setStart()
-				
+
 				location_truth = []
 				transition_truth = []
 				sensing_truth = []
-				
+
 				HeatSurface = drawHeatmap(HeatSurface,GridSurface,heatmap[1],1)
-				
+
 				print "Generated new map with blocks"
 			elif event.key == pygame.K_e:
 				agentx,agenty = setStart()
@@ -553,16 +514,190 @@ while(running):
 						mapfile.read(1)
 
 					mapfile.close()
-				
+
 				location_truth = []
 				transition_truth = []
 				sensing_truth = []
-				
+
 				heatmap = initHeatMap()
 				HeatSurface = drawHeatmap(HeatSurface,GridSurface,heatmap[test_time],test_time)
-				
+
 				GridSurface = drawGrid(GridSurface)
 				print "Map loaded!"
+			elif event.key == pygame.K_d:
+				print "hello"
+				filenumcount = 1
+				for i in range(1,11):
+					#load map
+					with open(os.path.join("./gen","map"+str(i)),"r") as mapfile: #changed to allow using /maps folder
+						new_start = make_tuple(mapfile.readline())
+						agentx = new_start[0]
+						agenty = new_start[1]
+
+						for y in range(len(grid[x])):				# Read each cell
+							for x in range(len(grid)):
+								grid[x][y].setType(mapfile.read(1))
+							mapfile.read(1)
+
+					GridSurface = drawGrid(GridSurface)
+					print "Map loaded!"
+					for j in range(0, 10):
+						agentx,agenty = setStart()
+						rand = 0
+						sensor = ""
+						celltype = ""
+						s = makeActions()
+						with open(os.path.join("./gen/ground_truths","ground_truth"+str(filenumcount)+".txt"), "w") as f:
+							f.write("("+str(agentx) + "," + str(agenty) + ")\n")
+							for c in s:
+								print c,
+								#time.sleep(0.5)
+								if c == 'U':
+									rand = random.randint(1,100)
+									#move according to 90/10 probability
+									if rand <= 90:
+										try:
+											if agenty-1 >= 0 and grid[agentx][agenty-1].getType() != 'B':		# Check for block or out of bounds
+												agenty -= 1
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+												print "agent x is " + str(agentx) + " agent y is " + str(agenty)
+											else:
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+										except IndexError:
+											agentx = agentx
+									else:
+										f.write("("+str(agentx) + "," + str(agenty)+")\n")
+									celltype = grid[agentx][agenty].getType()
+									rand = random.randint(1,100)
+									if rand <= 90:
+										sensor += celltype
+									elif rand > 90 and rand <= 95:
+										if celltype == 'N':
+											sensor += 'H'
+										elif celltype == 'T':
+											sensor += 'N'
+										else:
+											sensor += 'T'
+									else:
+										if celltype == 'N':
+											sensor += 'T'
+										elif celltype == 'T':
+											sensor += 'H'
+										else:
+											sensor += "N"
+								elif c == 'D':
+									rand = random.randint(1,100)
+									#move according to 90/10 probability
+									if rand <= 90:
+										try:
+											if agenty+1 < GridRows and grid[agentx][agenty+1].getType() != 'B':		# Check for block or out of bounds:
+												agenty += 1
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+												print "agent x is " + str(agentx) + " agent y is " + str(agenty)
+											else:
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+										except IndexError:
+											agentx = agentx
+									else:
+										f.write("("+str(agentx) + "," + str(agenty)+")\n")
+									celltype = grid[agentx][agenty].getType()
+									rand = random.randint(1,100)
+									if rand <= 90:
+										sensor += celltype
+									elif rand > 90 and rand <= 95:
+										if celltype == 'N':
+											sensor += 'H'
+										elif celltype == 'T':
+											sensor += 'N'
+										else:
+											sensor += 'T'
+									else:
+										if celltype == 'N':
+											sensor += 'T'
+										elif celltype == 'T':
+											sensor += 'H'
+										else:
+											sensor += "N"
+								elif c == 'L':
+									rand = random.randint(1,100)
+									#move according to 90/10 probability
+									if rand <= 90:
+										try:
+											if agentx-1 >= 0 and grid[agentx-1][agenty].getType() != 'B':		# Check for block or out of bounds:
+												agentx -= 1
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+												print "agent x is " + str(agentx) + " agent y is " + str(agenty)
+											else:
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+										except IndexError:
+												agentx = agentx
+									else:
+										f.write("("+str(agentx) + "," + str(agenty)+")\n")
+									celltype = grid[agentx][agenty].getType()
+									rand = random.randint(1,100)
+									if rand <= 90:
+										sensor += celltype
+									elif rand > 90 and rand <= 95:
+										if celltype == 'N':
+											sensor += 'H'
+										elif celltype == 'T':
+											sensor += 'N'
+										else:
+											sensor += 'T'
+									else:
+										if celltype == 'N':
+											sensor += 'T'
+										elif celltype == 'T':
+											sensor += 'H'
+										else:
+											sensor += "N"
+								elif c == 'R':
+									rand = random.randint(1,100)
+									#move according to 90/10 probability
+									if rand <= 90:
+										try:
+											if agentx+1 < GridCols and grid[agentx+1][agenty].getType() != 'B':		# Check for block or out of bounds
+												agentx += 1
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+												print "agent x is " + str(agentx) + " agent y is " + str(agenty)
+											else:
+												f.write("("+str(agentx) + "," + str(agenty)+")\n")
+										except IndexError:
+											agentx = agentx
+									else:
+										f.write("("+str(agentx) + "," + str(agenty)+"\n")
+									celltype = grid[agentx][agenty].getType()
+									rand = random.randint(1,100)
+									if rand <= 90:
+										sensor += celltype
+									elif rand > 90 and rand <= 95:
+										if celltype == 'N':
+											sensor += 'H'
+										elif celltype == 'T':
+											sensor += 'N'
+										else:
+											sensor += 'T'
+									else:
+										if celltype == 'N':
+											sensor += 'T'
+										elif celltype == 'T':
+											sensor += 'H'
+										else:
+											sensor += "N"
+							#else:
+								#print "Came across unknown value, exiting"
+								#exit(0)
+
+							f.write("Transition data:\n")
+							for c in s:
+								f.write(c+"\n")
+							f.write("Sensor data:\n")
+							for c in sensor:
+								f.write(c+"\n")
+							filenumcount += 1
+					print "agent x is " + str(agentx) + " agent y is " + str(agenty)
+					agentx, agenty = ground_truth_data1(s)
+					GridSurface = drawGrid(GridSurface)
 			elif event.key == pygame.K_c:
 				s = makeActions()
 				print "agent x is " + str(agentx) + " agent y is " + str(agenty)
@@ -575,7 +710,7 @@ while(running):
 				sensing_truth = []
 				heatmap = initHeatMap()
 				HeatSurface = drawHeatmap(HeatSurface,GridSurface,heatmap[test_time],test_time)
-				
+
 				# Load map: get filename
 				#filename = raw_input("Load truth file from: ")
 				filename = "ground_truth.txt"
@@ -584,43 +719,41 @@ while(running):
 					new_start = make_tuple(truthfile.readline())
 					start_x = new_start[0]
 					start_y = new_start[1]
-					
+
 					location_truth.append(new_start)
 					# Get true location data
 					for y in range(0,100):
 						location_truth.append(make_tuple(truthfile.readline()))
-					
+
 					truthfile.readline()			# Skip title of section
-					
+
 					goal_x = location_truth[99][0]
 					goal_y = location_truth[99][1]
-					
+
 					transition_truth.append("NA")
 					# Get true transition data
 					for y in range(0,100):				# Get true location data
 						transition_truth.append(truthfile.read(1))
 						truthfile.read(1)
-					
+
 					truthfile.readline()			# Skip title of section
-					
+
 					sensing_truth.append("NA")
 					# Get true sensing data
 					for y in range(0,100):				# Get true location data
 						sensing_truth.append(truthfile.read(1))
 						truthfile.read(1)
-					
+
 					truthfile.close()
 					print "Truth file loaded"
 			elif event.key == pygame.K_h:		# run truth file/generate heat map
 				#heatmap = initHeatMap()
 				#print str(transition_truth)
 				heatmap = forwardAlgorithm(transition_truth,sensing_truth)
-				
+
 				test_time = 1
 				HeatSurface = drawHeatmap(HeatSurface,GridSurface,heatmap[test_time],test_time)
 				print "Heat maps generated"
-			elif event.key == pygame.K_q:		# get error calculation for filtering (Question E)
-				myerror = error_filtering(heatmap,location_truth)
 
 	# Draw Screen
 	drawScreen(GridSurface,HeatSurface,test_time,location_truth,transition_truth,sensing_truth,start_x,start_y,goal_x,goal_y)
